6d035fd4d1990b681c9a6bfdb41f92de09884aa9
Kobe <dev.skyachieve91@gmail.com>
2024-08-02 07:35:12 +0900

    :sparkles: Feat: 'getCategoryType 메서드 생성 및 구현.'
    데이터베이스의 값과 열거형의 값이 항상 일치하지 않을 수 있으므로 'valueOf' 대신 더 유연한 로직 구현을 위해 'getCategoryType' 메서드를 구현하였습니다.

--- src/main/java/com/devkobe/portfolioBlog/dto/create/ContentResponseDto.java
+++ src/main/java/com/devkobe/portfolioBlog/dto/create/ContentResponseDto.java
@@ -1,6 +1,7 @@
 package com.devkobe.portfolioBlog.dto.create;
 
 import com.devkobe.portfolioBlog.domain.Content;
+import com.devkobe.portfolioBlog.dto.create.ContentResponseDto.CategoryType;
 import lombok.Getter;
 
 @Getter
@@ -13,17 +14,29 @@ public class ContentResponseDto {
 
     public ContentResponseDto(Content content) {
         this.representativeImageUrl = content.getRepresentativeImageUrl();
-        this.category = CategoryType.valueOf(content.getCategory());
+        this.category = getCategoryType(content.getCategory());
         this.title = content.getTitle();
         this.connectUrl = content.getConnectUrl();
     }
 
     public enum CategoryType {
+        DEFAULT,
         IOS,
         ANDROID,
         FRONTEND,
         BACKEND;
     }
+
+    public static CategoryType getCategoryType(String dbValue) {
+        for (CategoryType type : CategoryType.values()) {
+            if (type.name().equals(dbValue)) {
+                return type;
+            }
+        }
+        return CategoryType.DEFAULT;
+    }
 }
+
+
 
 
