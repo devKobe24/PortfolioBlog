02c452064f83fc54551aaf8da61485db554d0be3
Kobe <dev.skyachieve91@gmail.com>
2024-08-01 12:03:19 +0900

    :sparkles: Feat: 'findAllContents() 메서드 생성 및 구현.'
    /api/contents GET 요청이 오면 컨텐츠 전체를 조회하는 findAll() 메서드를 호출한 다음 응답용 객체인 ContentResponseDto로 파싱해 body에 담아 클라이언트에게 전송하는 코드를 스트림을 적용하여 구현하였습니다.

--- src/main/java/com/devkobe/portfolioBlog/controller/PortfolioApiController.java
+++ src/main/java/com/devkobe/portfolioBlog/controller/PortfolioApiController.java
@@ -2,10 +2,13 @@ package com.devkobe.portfolioBlog.controller;
 
 import com.devkobe.portfolioBlog.domain.Content;
 import com.devkobe.portfolioBlog.dto.create.AddContentRequestDto;
+import com.devkobe.portfolioBlog.dto.create.ContentResponseDto;
 import com.devkobe.portfolioBlog.service.PortfolioBlogService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,16 @@ public class PortfolioApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(savedContent);
+    }
+
+    @GetMapping("/api/contents")
+    public ResponseEntity<List<ContentResponseDto>> findAllContents() {
+        List<ContentResponseDto> contents = portfolioBlogService.findAll()
+            .stream()
+            .map(ContentResponseDto::new)
+            .toList();
+
+        return ResponseEntity.ok()
+            .body(contents);
     }
 }
