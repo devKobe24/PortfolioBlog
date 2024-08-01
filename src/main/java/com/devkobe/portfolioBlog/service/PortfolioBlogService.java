2a043046bc2a6345a236ca1a690cc5c30020eda6
Kobe <dev.skyachieve91@gmail.com>
2024-08-01 10:45:12 +0900

    :sparkles: Feat: 'findAll 메서드 생성 및 구현.'
    DB에 저장되어 있는 글을 모두 가져오는 findAll() 메서드를 추가하였습니다. JPA 지원 메서드인 findAll()을 호출해 content 테이블에 저장되어 있는 모든 데이터를 조회합니다.

--- src/main/java/com/devkobe/portfolioBlog/service/PortfolioBlogService.java
+++ src/main/java/com/devkobe/portfolioBlog/service/PortfolioBlogService.java
@@ -3,6 +3,7 @@ package com.devkobe.portfolioBlog.service;
 import com.devkobe.portfolioBlog.domain.Content;
 import com.devkobe.portfolioBlog.dto.create.AddContentRequestDto;
 import com.devkobe.portfolioBlog.repository.PortfolioBlogRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,10 @@ public class PortfolioBlogService {
     // 포트폴리오 컨텐츠 추가 메서드
     public Content save(AddContentRequestDto requestDto) {
         return portfolioBlogRepository.save(requestDto.toEntity());
+    }
+
+    // DB에 저장되어 있는 글을 모두 가져오는 메서드
+    public List<Content> findAll() {
+        return portfolioBlogRepository.findAll();
     }
 }
